8dcc1052e288ebd130e10f427b3da0a90f025fe7
Tom <elunico@users.noreply.github.com>
2021-06-29 07:58:02 -0400

    Released 0.2.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="csvtea-elunico",
-    version="0.1.0",
+    version="0.2.0",
     author="Thomas Povinelli",
     author_email="author@example.com",
     description="A flexible, extensible, pluggable, slightly-out-of-spec CSV parser",
